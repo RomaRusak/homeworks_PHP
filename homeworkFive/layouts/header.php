785c2bb36ee924f25e95b3c933460acd6705aee2
RomaRusak <109284248+RomaRusak@users.noreply.github.com>
2023-05-27 15:53:21 +0300

    complany logo in header is a link now

--- homeworkFive/layouts/header.php
+++ homeworkFive/layouts/header.php
@@ -8,15 +8,18 @@
     <link rel="stylesheet" href="../style/media.css">
 </head>
 
+<body>
+
 <header class="header">
     <div class="burger-menu">
         <div class="burger-item"></div>
         <div class="burger-item"></div>
         <div class="burger-item"></div>
     </div>
-    <img src="./img/Logo.png" class="logo" />
+    <a href="/">
+        <img src="./img/Logo.png" class="logo" />
+    </a>
     <?php require_once './components/nav.php' ?>
     <button class="get-call-button">Заказать звонок</button>
 </header>
 
-<body>
